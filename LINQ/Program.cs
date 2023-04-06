6c5f5a686f578b0c54ed243c02b6962c16e7305d
Autif Kamal <kautif@gmail.com>
2023-04-06 12:56:30 -0500

    games sorted by name length

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videogames = new List<string>();
+            videogames.Add("Mega Man X");
+            videogames.Add("Dark Souls 1");
+            videogames.Add("Hades");
+            videogames.Add("Super Mario World");
+            videogames.Add("Hollow Knight");
+
+            videogames.Sort((game1, game2) =>
+            {
+                Console.WriteLine($"Comparing {game1} with {game2}");
+                Console.WriteLine($"Game 1 Length: {game1.Length}, Game 2 Length: {game2.Length}");
+                return game1.Length.CompareTo(game2.Length);
+            });
         }
     }
 }
